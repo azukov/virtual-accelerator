56893d29cb35efbc9bb7dffd46150c48956169fb
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-10-11 15:11:26 -0400

    simplifying conda CI

--- .github/workflows/corrector-test.sh
+++ .github/workflows/corrector-test.sh
@@ -3,6 +3,8 @@ env
 pwd
 ls -alh
 sns_va &
-#python virtaccl/site/SNS_Linac/virtual_SNS_linac.py
-echo $(jobs -p)
+VA_PID=$(jobs -p)
 python virtaccl/examples/Corrector.py
+jobs -l
+kill $VA_PID
+jobs -l
