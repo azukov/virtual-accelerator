0784a1765ebf7b7097f34fe701904f393d54ed6a
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-10-11 23:34:46 -0400

    Switching to Pytest: bigger sleep time to wait for VA to start.

--- tests/sns_test.py
+++ tests/sns_test.py
@@ -3,7 +3,7 @@ from time import sleep
 import pytest
 import subprocess
 
-from epics import caget
+from epics import caget, caput
 
 
 @pytest.fixture(scope="session")
@@ -33,4 +33,32 @@ def test_bad_pv(va_process):
     assert va_process.poll() is None
     x = caget('BAD:PV:Name', connection_timeout=0.1)
     assert x is None
+
+
+def test_corrector(va_process):
+    corrector = "SCL_Mag:DCH00:B"
+    corrector_set = "SCL_Mag:PS_DCH00:B_Set"
+    bpm_device = "SCL_Diag:BPM04:xAvg"
+
+    original_val = caget(corrector_set)
+    settings = [(0.00, 0.0),
+                (0.02, 2.3),
+                (0.04, 5.0),
+                (0.06, 7.7),
+                (0.08, 10.0)]
+
+    for b_set, bpm in settings:
+
+        caput(corrector_set, b_set)
+        sleep(1.5)
+        b_reading = caget(corrector)
+        bpm_reading = caget(bpm_device)
+
+        print(f'Corrector value: {b_reading:.4f}/{b_set:.4f}')
+        assert b_reading == pytest.approx(b_set, abs=0.001)
+
+        print(f'BPM value: {bpm_reading:.4f}/{bpm:.4f}')
+        assert bpm_reading == pytest.approx(bpm, abs=0.1)
+
+    caput(corrector_set, original_val)
 
