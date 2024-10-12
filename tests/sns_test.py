999be643c49e1577662782263e71fe2250a70d23
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-10-11 22:48:20 -0400

    Switching to Pytest

---
+++ tests/sns_test.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+import pytest
+import subprocess
+
+from epics import caget
+
+
+@pytest.fixture(scope="session")
+def va_process():
+    # Start VA as a background process
+    proc = subprocess.Popen(["sns_va"])
+
+    # Wait for the VA to start serving PVs
+    sleep(1.0)
+    print('VA should be ready by now')
+    yield proc
+
+    # Tear down: stop the background process when tests are done
+    proc.terminate()
+    proc.wait()
+
+
+def test_pv_connection(va_process):
+
+    assert va_process.poll() is None
+    x = caget('SCL_Diag:BPM04:xAvg', connection_timeout=0.1)
+    assert x is not None
+
+
+def test_bad_pv(va_process):
+
+    assert va_process.poll() is None
+    x = caget('BAD:PV:Name', connection_timeout=0.1)
+    assert x is None
+
